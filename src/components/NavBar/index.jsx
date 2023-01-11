18cb88263d5c0e66244b5d2594d8b124e030a98f
Diego Figueirêdo <8kball@gmail.com>
2023-01-11 18:35:59 -0300

    style: adjust nav spacing and add santa component

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,10 @@
 import "./style.css";
+import NavSanta from "../NavSanta";
 
 const NavBar = () => {
   return (
     <nav>
+      <NavSanta />
       <span>Feliz Natal</span>
       <span>Início</span>
       <span>Acessórios</span>
