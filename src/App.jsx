3bdc74fbff0ecf1d7c669d7f79297f1a68a84665
Diego Figueirêdo <8kball@gmail.com>
2023-01-11 16:41:50 -0300

    feat: add navbar with base style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from './components/Header';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header>
+        <NavBar />
+      </Header>
     </div>
   )
 }
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <span>Feliz Natal</span>
+      <span>Início</span>
+      <span>Acessórios</span>
+      <span>Doação</span>
+      <span>Contato</span>
+    </nav>
+  )
+};
+
+export default NavBar;
