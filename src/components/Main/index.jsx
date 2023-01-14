530a923b072f3e7e35610f7de16aa732fe98ff80
Diego Figueirêdo <8kball@gmail.com>
2023-01-14 12:05:18 -0300

    feat: add bottom-section

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,7 +3,7 @@ import "./style.css";
 const Main = () => {
   return (
     <main>
-      <section>
+      <section className="upper-section">
         <div className="heading">
           <h2>Acessórios</h2>
           <p>
@@ -40,6 +40,18 @@ const Main = () => {
               Aproveite os sentimentos que duram para sempre.
             </p>
           </div>
+        </div>
+      </section>
+      <section className="bottom-section">
+        <img src="/bottom-section-gift.png" alt="" />
+        <div className="heading-wrapper">
+          <h1>Comemore com muito amor!</h1>
+          <p>
+            Nestes feriados, festeje com muito amor e paz,
+            oferecendo muitas bênçãos aos nossos entes queridos,
+            amigos e vizinhos, desejando-lhes um Feliz Natal.
+          </p>
+          <button>Fazer doação</button>
         </div>
       </section>
     </main>
